8e6e0ac0e602395c8cdc7d495b2f3899077fbcff
AK-ik77 <5t774cc55r@privaterelay.appleid.com>
2026-06-15 19:25:15 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,8 +2,8 @@ window.FIFA_CONFIG = {
   VERSION: 'V10.4.2 公测版',
   TITLE: '2026FIFA世界杯预测',
 
-  SUPABASE_URL: "https://ovmlorwcjodcrocmxzva.supabase.co";
-  SUPABASE_ANON_KEY:"sb_publishable_4txLl-xZg_wqw8I7WIH4Ig_eGQ7AR4f";
+  SUPABASE_URL: 'https://ovmlorwcjodcrocmxzva.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_4txLl-xZg_wqw8I7WIH4Ig_eGQ7AR4f',
 
   SUPABASE_TABLE: 'matches',
   REQUEST_TIMEOUT_MS: 4000,
